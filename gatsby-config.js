4e5a9075127e8706fd4df9775250eabffc7bb945
hugo humblebee <hugo.hedelin@humblebee.se>
2020-05-17 12:55:46 +0200

    Added theme ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,21 +2,23 @@ require("dotenv").config();
 
 module.exports = {
   siteMetadata: {
-    siteName: 'My Shop',
+    siteName: "My Shop",
   },
   plugins: [
-    'gatsby-plugin-sass',
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-theme-ui",
     {
       resolve: `gatsby-source-datocms`,
       options: { apiToken: process.env.DATO_API_TOKEN },
     },
     {
-      resolve: 'gatsby-plugin-snipcart',
+      resolve: "gatsby-plugin-snipcart",
       options: {
-        apiKey: 'OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1',
-        autopop: true
-      }
+        apiKey:
+          "OWE3MmZmMjQtNTk3Yi00OThhLWEwMmUtZDY4ZWM4NzIwYzZiNjM2NjM0Mzc1NzE0MTUwNzI1",
+        autopop: true,
+      },
     },
   ],
-}
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
-import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import Layout from "../layouts/index"
-import Img from 'gatsby-image'
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+import Layout from "../layouts/index";
+import Img from "gatsby-image";
+import Header from "../components/UI/Header";
+import { Box, Flex, Heading, Button } from "theme-ui";
 
 export default () => (
   <StaticQuery
@@ -29,37 +31,46 @@ export default () => (
         }
       }
     `}
-render={data => (
-  <Layout site={data.site}>
-    <div className="Catalogue">
-      {
-        data.products.edges.map(({ node: product }) => (
-          <div className="Catalogue__item" key={product.id}>
-            <div
-              className="Product snipcart-add-item"
-              data-item-id={product.id}
-              data-item-price={product.price}
-              data-item-image={product.image.url}
-              data-item-name={product.name}
-              data-item-url={`/`}
+    render={(data) => (
+      <Layout site={data.site}>
+        <Header>
+          <Box p={4} bg="highlight">
+            <Flex
+              sx={{
+                alignItems: "center",
+              }}
             >
-              <div className="Product__image">
-                <Img sizes={product.image.sizes} />
-              </div> <div className="Product__details">
-                <div className="Product__name">
-                  {product.name}
-                  <div className="Product__price">
-                    {product.price}€
+              <Heading>Components</Heading>
+              <Button ml="auto">Beep</Button>
+            </Flex>
+          </Box>
+        </Header>
+        <div className="Catalogue">
+          {data.products.edges.map(({ node: product }) => (
+            <div className="Catalogue__item" key={product.id}>
+              <div
+                className="Product snipcart-add-item"
+                data-item-id={product.id}
+                data-item-price={product.price}
+                data-item-image={product.image.url}
+                data-item-name={product.name}
+                data-item-url={`/`}
+              >
+                <div className="Product__image">
+                  <Img sizes={product.image.sizes} />
+                </div>{" "}
+                <div className="Product__details">
+                  <div className="Product__name">
+                    {product.name}
+                    <div className="Product__price">{product.price}€</div>
                   </div>
+                  <span className="Product__buy">Buy now</span>
                 </div>
-                <span className="Product__buy">Buy now</span>
               </div>
             </div>
-          </div>
-        ))
-      }
-    </div>
-  </Layout>
-     )}
-   />
-)
+          ))}
+        </div>
+      </Layout>
+    )}
+  />
+);
